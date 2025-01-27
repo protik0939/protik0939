929e145b38b93ac3a33109d6612a9dc049d97668
protik0939 <protik0939@gmail.com>
2025-01-27 17:20:09 +0600

    bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,31 @@ import Head from "next/head";
 export const metadata: Metadata = {
   title: "Home | Sadat Alam Protik",
   description: "Oh, oh woe-oh-woah is me 🖤😉",
+  keywords: "Sadat Alam Protik, portfolio, developer, React, Next.js",
+  openGraph: {
+    title: "Sadat Alam Protik",
+    description: "Portfolio of Sadat Alam Protik",
+    url: "https://yourwebsite.com",
+    siteName: "Sadat's Portfolio",
+    images: [
+      {
+        url: "/protikWOutBg.webp",
+        width: 800,
+        height: 800,
+        alt: "Protik's Portfolio Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
+
 
 const balooDaTwo = Baloo_Da_2({
   subsets: ['latin'],
   weight: ['400'],
   variable: '--font-baloo-da-2'
 })
-
-
-export const gscVerification: Metadata = {
-  title: "google-site-verification",
-  description: "eIF-DBTlpUgbA88_sV07lhJiT8iX_luc8VqUGSfG8lc",
-};
 
 export default function RootLayout({
   children,
@@ -36,7 +48,7 @@ export default function RootLayout({
           type="image/<generated>"
           sizes="<generated>"
         />
-        
+        <meta name="google-site-verification" content="eIF-DBTlpUgbA88_sV07lhJiT8iX_luc8VqUGSfG8lc" />
       </Head>
       <body
         className={` ${balooDaTwo.variable} antialiased`} >
